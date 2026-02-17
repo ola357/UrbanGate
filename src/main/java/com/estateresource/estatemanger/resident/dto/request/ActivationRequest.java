ece40f1648caf423635a7f456daaa3cc899f4060
javapiest <enwerevincent@gmail.com>
2026-02-17 14:11:55 +0100

    Add Swagger, API Request Validations and Exception Handler

--- src/main/java/com/estateresource/estatemanger/resident/dto/request/ActivationRequest.java
+++ src/main/java/com/estateresource/estatemanger/resident/dto/request/ActivationRequest.java
@@ -1,4 +1,9 @@
 package com.estateresource.estatemanger.resident.dto.request;
 
-public record ActivationRequest(String token) {
+import jakarta.validation.constraints.NotBlank;
+
+public record ActivationRequest(
+        @NotBlank(message = "Token Cannot be Blank")
+        String token
+) {
 }
