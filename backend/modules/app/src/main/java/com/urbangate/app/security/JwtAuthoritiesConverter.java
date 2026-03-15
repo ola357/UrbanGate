44df84602f9519baf3c9f650e1633f1307bddf34
Alli Olaoluwa <alliolaoluwa@gmail.com>
2026-03-15 17:02:29 +0100

    OLAM-0: set up web scaffold

--- backend/modules/app/src/main/java/com/urbangate/app/security/JwtAuthoritiesConverter.java
+++ backend/modules/app/src/main/java/com/urbangate/app/security/JwtAuthoritiesConverter.java
@@ -5,8 +5,8 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import org.springframework.core.convert.converter.Converter;
-import org.springframework.lang.Nullable;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
@@ -18,11 +18,8 @@ public class JwtAuthoritiesConverter implements Converter<Jwt, Collection<Grante
   private static final String CLIENT_ID = "ug-api";
 
   @Override
-  public Collection<GrantedAuthority> convert(@Nullable Jwt jwt) {
-    if (jwt == null) {
-      return List.of();
-    }
-
+  public Collection<GrantedAuthority> convert(Jwt jwt) {
+    Objects.requireNonNull(jwt, "jwt must not be null");
     List<GrantedAuthority> authorities = new ArrayList<>();
 
     // Realm roles
